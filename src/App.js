5bdf598a29d94e8d19e2eb60badbedeea0785734
jvtrf <joaovitorfagundes@hotmail.com>
2022-07-28 15:53:34 -0300

    Page begin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import { About, Home } from "./NavComponents";
+import "./App.css";
 
 function App() {
+  const [loadedComponent, setLoadedComponent] = useState("Home");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="main-content">
+      <header>
+        <div className="logo">Jvtrf</div>
+        <div className="menu">
+          <nav>
+            <button
+              className="menu-link"
+              onClick={() => setLoadedComponent("Home")}
+            >
+              <i className="fa-solid fa-house"></i>Home
+            </button>
+            <button
+              className="menu-link"
+              onClick={() => setLoadedComponent("About")}
+            >
+              <i className="fa-solid fa-user"></i>About
+            </button>
+            <a
+              className="menu-link"
+              href="https://github.com/jvtrf"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github"></i>Git
+            </a>
+            <a
+              className="menu-link"
+              href="https://www.linkedin.com/in/jvtrf/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-linkedin"></i>Linkedin
+            </a>
+
+            <div id="contact-drop">
+              <div className="menu-link">
+                <i className="fa-solid fa-envelope"></i>Contact
+              </div>
+              <div id="contact-drop-content">
+                <a
+                  className="menu-link"
+                  href="https://api.whatsapp.com/send?phone=5562981488991"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className="fa-brands fa-whatsapp"></i>Whatsapp
+                </a>
+                <a
+                  className="menu-link"
+                  href="https://t.me/+5562981488991"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i class="fa-brands fa-telegram"></i>Telegram
+                </a>
+                <a className="menu-link" href="#">
+                  <i className="fa-brands fa-instagram"></i>Instagram
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
       </header>
+      {loadedComponent === "Home" ? <Home /> : ""}
+      {loadedComponent === "About" ? <About /> : ""}
     </div>
   );
 }
---
+++ src/NavComponents.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return <div className="main-content"></div>;
+};
+
+const About = () => {
+  return <div className="main-content"></div>;
+};
+
+export { Home, About };
